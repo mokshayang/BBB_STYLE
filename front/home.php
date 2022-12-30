e5f9dd825b216fcac4c013252baa0f9228ac8cec
moksha <dullstart.yang@gmail.com>
2022-12-30 15:39:08 +0800

    最新消息的顯示 不超過五筆

--- front/home.php
+++ front/home.php
@@ -41,7 +41,16 @@
 
 
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
-        <span class="t botli">最新消息區     </span>
+        <span class="t botli">最新消息區     
+            <?php
+                if($News->count(['sh'=>1])>5){
+                    echo "<a href='?do=news' style='float:right;'>";
+                    echo "More...";
+                    echo "</a>";
+                }
+
+            ?>
+        </span>
         <ul class="ssaa" style="list-style-type:decimal;">
         <?php
         $news = $News->all(['sh'=>1],"LIMIT 5");
